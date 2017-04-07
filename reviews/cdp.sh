5233ba656633dd9fa76784e04b83301206cc8ed9
Jens Fischer <j.fischer@epages.com>
2017-04-07 13:59:10 +0200

    also include total number of deployments

--- reviews/cdp.sh
+++ reviews/cdp.sh
@@ -3,9 +3,9 @@
 if [ "$#" -ne 2 ] ;then
     cat <<EOF
 Usage
-    ./cdp.sh <DATE> <TIME>
+    ./cdp.sh <DATE> <TIME> | xmllint --format -
 Examples
-  ./cdp.sh 20170328 1300
+  ./cdp.sh 20170328 1300 | xmllint --format -
 EOF
     exit 1
 fi
@@ -16,7 +16,22 @@ else
   SINCE=$(date --utc -d "$1 $2" +%s)
 fi
 
-http --verify no --body https://cdp.epages.works/job/cdp/api/json tree==allBuilds[timestamp,result,description] | \
-jq --arg since $SINCE '.allBuilds | map(select(.timestamp >= ($since | tonumber))) | map(select(.result | tostring | contains("SUCCESS"))) | map({repo: .description | split(" ") | (.[0])}) | group_by(.repo) | map( {"repo": .[0].repo, "deployments":length } ) | sort_by(-.deployments, .repo)' | \
-jq -r '.[] | [.repo, .deployments] | @csv' | \
-awk -v FS="," 'BEGIN{print "<table class=\"cdp\">"; print "  <thead>"; print"    <tr><th>repo</th><th>deployments</th></tr>"; print "  </thead>"; print "  <tbody>"} {gsub ("\"",""); printf "    <tr><td>%s</td><td>%s</td></tr>%s",$1,$2,ORS} END{print "  </tbody>"; print "</table>"}'
+# fetch build history from CDP Jenkins
+CDP=$(http --verify no --body https://cdp.epages.works/job/cdp/api/json tree==allBuilds[timestamp,result,description])
+
+# filter newest successful builds and transform their JSON structure by splitting off the repo name from description
+# sort the filtered list by number of deployments, highest first
+DEPLOYMENTS=$(echo $CDP | \
+  jq --arg since $SINCE '.allBuilds | map(select(.timestamp >= ($since | tonumber))) | map(select(.result | tostring | contains("SUCCESS"))) | map({repo: .description | split(" ") | (.[0])}) | group_by(.repo) | map( {"repo": .[0].repo, "deployments":length } ) | sort_by(-.deployments, .repo)')
+
+# calculate the total number of all deployments
+export TOTAL=$(echo $DEPLOYMENTS | \
+  jq 'map(to_entries) | add | group_by(.key) | map({key: .[0].key, value: map(.value) | add }) | from_entries | .deployments'
+)
+
+# convert the JSON to CSV and print it as HTML table
+TABLE=$(echo $DEPLOYMENTS | jq -r '.[] | [.repo, .deployments] | @csv' | \
+  awk -v FS="," 'BEGIN{print "<table class=\"cdp\">"; print "<thead>"; print "<tr><th>repo</th><th>deployments</th></tr>"; print "</thead>"; print "<tbody>"} {gsub ("\"",""); printf "<tr><td>%s</td><td>%s</td></tr>%s",$1,$2,ORS} END{print "<tr class=\"all\"><td>all</td><td>"; print ENVIRON["TOTAL"]; print "</td></tr>";print "</tbody>"; print "</table>"}')
+
+echo $TABLE
+echo
